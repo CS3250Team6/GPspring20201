85d19854bbbf2863cee13fc2450e1749a2f8dca9
hunter-holton <hunter.holton>
2021-02-04 15:56:04 -0700

    added in csv reader

--- GroupProject.java
+++ GroupProject.java
@@ -1,9 +1,16 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class GroupProject {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		Scanner sc = new Scanner(new File("inventory_team6.csv"));   
+		sc.useDelimiter(",");
 
+		while(sc.hasNext()) {
+			System.out.println(sc.next());
+		}
 	}
-
 }
