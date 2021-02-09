b4591e1e03e9e68bde9446dedd193ed045c929e2
hunter-holton <hunter.holton>
2021-02-09 00:02:58 -0700

    changed from array to array list to make it work

--- GroupProject.java
+++ GroupProject.java
@@ -1,16 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
+
 public class GroupProject {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		Scanner sc = new Scanner(new File("inventory_team6.csv"));   
+		
+		Scanner sc = new Scanner(new File("inventory_team6.csv"));
 		sc.useDelimiter(",");
+		String[][] data = new String[42776][6];
+		int i = 0;
+		int j = 0;
+		while (sc.hasNext()) {
+			j = 0;
+			i++;
 
-		while(sc.hasNext()) {
-			System.out.println(sc.next());
+			while (j < 5) {
+				j++;
+				data[i][j] = sc.next();
+			}
 		}
+		System.out.println( Arrays.deepToString(data));
 	}
 }
