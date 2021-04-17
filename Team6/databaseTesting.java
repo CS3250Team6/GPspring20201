f44316daf12a8922b2e91166d26067dd6686cf14
esmae <esmae@DESKTOP-OECN40F.Home>
2021-04-17 07:25:16 -0600

    This code will test the database schema stracture as well as the data entry.

---
+++ Team6/databaseTesting.java
@@ -0,0 +1,19 @@
+/* database Testing is important in software testing because it ensures
+* data values and information recived and stored into database are valid
+* not.data base testing helps to save data loss, saves abroted transaction data and no
+* no unauthorized acces to the information.
+*/
+import.java.unitil*;
+public class databaseTesting{
+       databaseTesting test = new databaseTesting();
+       test.prodact_id;
+       test.quantity;
+       test.wholsale_cost;
+       test.sale_price:
+       test.supplier_id
+     
+   public databaseTesting{
+      productid_id = Null;
+      
+   }
+}
